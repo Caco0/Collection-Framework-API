aff2011b4a497715c97560dd1db205daa51a51a0
Caco0 <cacotpds@gmail.com>
2024-03-29 09:22:30 -0300

    add CatalogBooks, Books, and logic classes for searching classes

---
+++ collections-java-api-2024/src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,67 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+
+    private List<Livro> livrosList;
+
+    public CatalogoLivros() {
+        this.livrosList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livrosList.add(new Livro(titulo, autor, anoPublicacao));
+
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+
+            List<Livro> livrosPorAutor = new ArrayList<>();
+
+            if(!livrosList.isEmpty()){
+                for(Livro l: livrosList) {
+                    if (l.getAutor().equalsIgnoreCase(autor)) {
+                        livrosPorAutor.add(l);
+                    }
+            }
+        }
+        return livrosPorAutor;
+    }
+    public List<Livro> pesqisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAno = new ArrayList<>();
+        if(!livrosList.isEmpty()){
+            for(Livro l : livrosList){
+                if(l.getAnoPublicacao()>= anoInicial && l.getAnoPublicacao()<= anoFinal){
+                    livrosPorIntervaloAno.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAno;
+    }
+    public Livro pesquisarPorTitulo(String titulo){
+            Livro livroEncontrado= null;
+            if( !livrosList.isEmpty()) {
+                for (Livro l : livrosList) {
+                    if (l.getTitulo().equalsIgnoreCase(titulo)){
+                        livroEncontrado = l;
+                        break;
+                }
+            }
+        }
+    return livroEncontrado;
+    }
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros() ;
+        catalogoLivros.adicionarLivro("Os Lusíadas", "Luís Vaz de Camões", 1556);
+        catalogoLivros.adicionarLivro("Os Lusíadas", "Pero Vaz Caminha", 1600);
+        catalogoLivros.adicionarLivro("Macunaíma", "Mario de Andrade", 1928);
+        catalogoLivros.adicionarLivro("Memórias Póstumas de Brás Cubas", "Mario de Andrade", 1959);
+        catalogoLivros.adicionarLivro("Memórias Póstumas de Brás Cubas", "Machado de Assis", 1881);
+        catalogoLivros.adicionarLivro("teste", "teste", 1881);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("teste"));
+    }
+    
+}
